82bf1a7317054f18d8fcff122d47985c3f5cf714
jose.parreno-dd <jose.parreno@digitaldimension.es>
2019-02-06 13:35:06 +0100

    Refactor person from SFC to Class component due to problem with enzyme.

--- src/components/main/person-card/Person.js
+++ src/components/main/person-card/Person.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import './Person.css'
+import Checkbox from "@material-ui/core/Checkbox/Checkbox";
 
-const Person = (props) => {
-    const {person} = props;
-    return (
-        <div className="card">
-            <img src={person.picture} alt="Avatar" className={"dynamicAvatar"}/>
-            <div className="container">
-                <h4><b>{person.name}</b></h4>
-                <p>{person.address}</p>
-                <input type={"checkbox"} onChange={() => props.handleCheckboxChange(props.person._id)}></input>
+class Person extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+    };
+
+    render() {
+        return (
+            <div className="card">
+                <img src={person.picture} alt="Avatar" className={"dynamicAvatar"}/>
+                <div className="container">
+                    <h4><b>{person.name}</b></h4>
+                    <p>{person.address}</p>
+                    <Checkbox></Checkbox>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 };
 
 export default Person;
