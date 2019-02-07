ffdca7a13da7e498b7c57dea9ecdf09d580cbf76
jose.parreno-dd <jose.parreno@digitaldimension.es>
2019-02-07 12:51:54 +0100

    Added shallow tests as well as some propTypes

--- src/components/__tests__/person.test.js
+++ src/components/__tests__/person.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {shallow} from '../../enzyme';
+import {shallow} from 'enzyme';
 
 import Person from '../main/person-card/Person';
 
@@ -11,16 +11,24 @@ describe('Person component', () => {
             "_id": "5c20f8aa7960361f6ecb3ed6",
             "index": 1,
             "greeting": "Hello, Jopi! You have 10 unread messages.",
-            "favoriteFruit": "strawberry"
+            "favoriteFruit": "strawberry",
+            "name": "Jopi Piqueras",
+            "address": "Smin Streen 78373",
+            "picture": "https://api.adorable.io/avatars/66/lacynguyen@nutralab.com"
         };
         const handleCheckboxChange = () => {
         };
 
         //Act
-        const wrapper = shallow(<Person person={jopi}
+        const personWrapper = shallow(<Person person={jopi}
                                         key={jopi._id}
                                         handleCheckboxChange={handleCheckboxChange}/>);
 
         //Assert
+        expect(personWrapper.find('h4')).toHaveLength(1);
+        expect(personWrapper.find('h4').text()).toBe("Jopi Piqueras");
+        expect(personWrapper.find('p').text()).toBe("Smin Streen 78373");
+        expect(personWrapper.find('img')).toHaveLength(1);
+        expect(personWrapper.find('img').prop("src")).toBe("https://api.adorable.io/avatars/66/lacynguyen@nutralab.com");
     });
 });
--- src/components/main/person-card/Person.js
+++ src/components/main/person-card/Person.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Person.css'
-import Checkbox from "@material-ui/core/Checkbox/Checkbox";
+import Checkbox from '@material-ui/core/Checkbox/Checkbox';
+import PropTypes from 'prop-types';
 
 class Person extends React.Component {
 
@@ -9,7 +10,8 @@ class Person extends React.Component {
         this.state = {};
     };
 
-    render() {
+    render(props) {
+        const {person} = this.props;
         return (
             <div className="card">
                 <img src={person.picture} alt="Avatar" className={"dynamicAvatar"}/>
@@ -22,5 +24,25 @@ class Person extends React.Component {
         )
     }
 };
+
+const personShape = PropTypes.shape({
+    "_id": PropTypes.string,
+    "index": PropTypes.number,
+    "guid": PropTypes.string,
+    "picture": PropTypes.string,
+    "age": PropTypes.number,
+    "eyeColor": PropTypes.string,
+    "name": PropTypes.string,
+    "email": PropTypes.string,
+    "address": PropTypes.string,
+    "greeting": PropTypes.string,
+    "favoriteFruit": PropTypes.string
+
+});
+
+Person.propTypes ={
+    person : personShape
+};
+
 
 export default Person;
