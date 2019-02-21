394623b1c9972f4d77f9642358dfcc8cf59488cd
jose.parreno-dd <jose.parreno@digitaldimension.es>
2019-02-21 17:35:45 +0100

    Added the snapshot testing

--- src/components/__tests__/person.test.js
+++ src/components/__tests__/person.test.js
@@ -5,6 +5,29 @@ import Person from '../main/person-card/Person';
 
 describe('Person component', () => {
 
+    it('should render correctly with specific props', () => {
+        //Arrange
+        const jopi = {
+            "_id": "5c20f8aa7960361f6ecb3ed6",
+            "index": 1,
+            "greeting": "Hello, Jopi! You have 10 unread messages.",
+            "favoriteFruit": "strawberry",
+            "name": "Jopi Piqueras",
+            "address": "Smin Streen 78373",
+            "picture": "https://api.adorable.io/avatars/66/lacynguyen@nutralab.com"
+        };
+        const handleCheckboxChange = () => {
+        };
+
+        //Act
+        const personWrapper = shallow(<Person person={jopi}
+                                              key={jopi._id}
+                                              handleCheckboxChange={handleCheckboxChange}/>);
+
+        //Assert
+        expect(personWrapper).toMatchSnapshot();
+    });
+
     it('should render a person card containing the model information', () => {
         //Arrange
         const jopi = {
